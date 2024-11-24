e2fcc836bc111c07a4bdd6b0ec2e287cf6efe698
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:36:17 +0330

    Update UiState.kt

--- domain/src/main/java/ir/mytodo/domain/model/base/UiState.kt
+++ domain/src/main/java/ir/mytodo/domain/model/base/UiState.kt
@@ -1,1 +1,8 @@
-package ir.mytodo.domain.model.basesealed class UiState<out T>{    data object Idle : UiState<Nothing>()    data object Loading : UiState<Nothing>()    data class Error(val message : String ?= null) : UiState<Nothing>()    data class Success<out T>(val resultData : T) : UiState<T>()}
+package ir.mytodo.domain.model.base
+
+sealed class UiState<out T>{
+    data object Idle : UiState<Nothing>()
+    data object Loading : UiState<Nothing>()
+    data class Error(val message : String ?= null) : UiState<Nothing>()
+    data class Success<out T>(val resultData : T) : UiState<T>()
+}
