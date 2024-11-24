a50ca98edb28dda456e99627bbfd25fd138524e3
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:35:22 +0330

    Update GetTodoListUseCase.kt

--- domain/src/main/java/ir/mytodo/domain/usecase/GetTodoListUseCase.kt
+++ domain/src/main/java/ir/mytodo/domain/usecase/GetTodoListUseCase.kt
@@ -1,1 +1,19 @@
-package ir.mytodo.domain.usecaseimport ir.mytodo.domain.model.Todoimport ir.mytodo.domain.model.base.ResultEntityimport ir.mytodo.domain.repositories.TodoRepositoryimport kotlinx.coroutines.CoroutineDispatcherimport kotlinx.coroutines.flow.Flowclass GetTodoListUseCase(    private val todoRepository: TodoRepository ,    coroutineDispatcher: CoroutineDispatcher) : BaseUseCase<Unit , ResultEntity<List<Todo>>>(coroutineDispatcher) {    override fun execute(parameters: Unit): Flow<ResultEntity<List<Todo>>> {        return todoRepository.getList()    }}
+package ir.mytodo.domain.usecase
+
+import ir.mytodo.domain.model.Todo
+import ir.mytodo.domain.model.base.ResultEntity
+import ir.mytodo.domain.repositories.TodoRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+
+class GetTodoListUseCase(
+    private val todoRepository: TodoRepository ,
+    coroutineDispatcher: CoroutineDispatcher
+) : BaseUseCase<Unit , ResultEntity<List<Todo>>>(coroutineDispatcher) {
+
+    override fun execute(parameters: Unit): Flow<ResultEntity<List<Todo>>> {
+        return todoRepository.getList()
+    }
+
+
+}
