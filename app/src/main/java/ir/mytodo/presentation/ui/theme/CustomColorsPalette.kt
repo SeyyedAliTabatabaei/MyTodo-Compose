0d68ea2713d13f4ba222091e6b2c15592df23237
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:31:36 +0330

    Update CustomColorsPalette.kt

--- app/src/main/java/ir/mytodo/presentation/ui/theme/CustomColorsPalette.kt
+++ app/src/main/java/ir/mytodo/presentation/ui/theme/CustomColorsPalette.kt
@@ -1,1 +1,26 @@
-package ir.mytodo.presentation.ui.themeimport androidx.compose.foundation.isSystemInDarkThemeimport androidx.compose.material3.ColorSchemeimport androidx.compose.runtime.Composableimport androidx.compose.runtime.Immutableimport androidx.compose.ui.graphics.Color@Immutabledata class CustomColorsPalette(    val textColorSecondary : Color = Color.Unspecified ,    val textColor : Color = Color.Unspecified ,    val backgroundCardColor : Color = Color.Unspecified ,)val LightCustomColorsPalette = CustomColorsPalette(    textColorSecondary = LightTextColorSecondary ,    textColor = LightTextColor ,    backgroundCardColor = LightCardBackground)val DarkCustomColorsPalette = CustomColorsPalette(    textColorSecondary = DarkTextColorSecondary ,    textColor = DarkTextColor ,    backgroundCardColor = DarkCardBackground)
+package ir.mytodo.presentation.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+
+@Immutable
+data class CustomColorsPalette(
+    val textColorSecondary : Color = Color.Unspecified ,
+    val textColor : Color = Color.Unspecified ,
+    val backgroundCardColor : Color = Color.Unspecified ,
+)
+
+val LightCustomColorsPalette = CustomColorsPalette(
+    textColorSecondary = LightTextColorSecondary ,
+    textColor = LightTextColor ,
+    backgroundCardColor = LightCardBackground
+)
+
+val DarkCustomColorsPalette = CustomColorsPalette(
+    textColorSecondary = DarkTextColorSecondary ,
+    textColor = DarkTextColor ,
+    backgroundCardColor = DarkCardBackground
+)
