cbf89b4a60d822e5316f084da7103b92288140db
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:31:59 +0330

    Update Utils.kt

--- app/src/main/java/ir/mytodo/presentation/utilities/Utils.kt
+++ app/src/main/java/ir/mytodo/presentation/utilities/Utils.kt
@@ -1,1 +1,6 @@
-package ir.mytodo.presentation.utilitiesimport androidx.compose.material.icons.Iconsimport androidx.compose.material.icons.filled.Addimport androidx.compose.ui.graphics.vector.ImageVectorimport kotlin.reflect.KProperty1
+package ir.mytodo.presentation.utilities
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.ui.graphics.vector.ImageVector
+import kotlin.reflect.KProperty1
