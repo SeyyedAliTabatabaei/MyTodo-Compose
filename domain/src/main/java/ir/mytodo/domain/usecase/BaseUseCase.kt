c6225384b9c17fa8d5cd9af47eb924dcc6f1deac
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:35:40 +0330

    Update BaseUseCase.kt

--- domain/src/main/java/ir/mytodo/domain/usecase/BaseUseCase.kt
+++ domain/src/main/java/ir/mytodo/domain/usecase/BaseUseCase.kt
@@ -1,1 +1,15 @@
-package ir.mytodo.domain.usecaseimport kotlinx.coroutines.CoroutineDispatcherimport kotlinx.coroutines.flow.Flowimport kotlinx.coroutines.flow.flowOnabstract class BaseUseCase<in P , R>(private val coroutineDispatcher : CoroutineDispatcher) {    operator fun invoke(parameters : P) : Flow<R> {        return execute(parameters)            .flowOn(coroutineDispatcher)    }    abstract fun execute(parameters: P) : Flow<R>}
+package ir.mytodo.domain.usecase
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+
+abstract class BaseUseCase<in P , R>(private val coroutineDispatcher : CoroutineDispatcher) {
+
+    operator fun invoke(parameters : P) : Flow<R> {
+        return execute(parameters)
+            .flowOn(coroutineDispatcher)
+    }
+
+    abstract fun execute(parameters: P) : Flow<R>
+}
