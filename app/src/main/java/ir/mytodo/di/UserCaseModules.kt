1f7520e093e4ecce597e6479daf760a1769fd01d
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:29:28 +0330

    Update UserCaseModules.kt

--- app/src/main/java/ir/mytodo/di/UserCaseModules.kt
+++ app/src/main/java/ir/mytodo/di/UserCaseModules.kt
@@ -1,1 +1,36 @@
-package ir.mytodo.diimport dagger.Moduleimport dagger.Providesimport dagger.hilt.InstallInimport dagger.hilt.components.SingletonComponentimport ir.mytodo.domain.repositories.TodoRepositoryimport ir.mytodo.domain.usecase.UpsertTodoUseCaseimport ir.mytodo.domain.usecase.DeleteTodoUseCaseimport ir.mytodo.domain.usecase.GetTodoListUseCaseimport kotlinx.coroutines.Dispatchersimport javax.inject.Singleton@Module@InstallIn(SingletonComponent::class)object UserCaseModules {    @Singleton    @Provides    fun provideUpsertTodoUseCase(todoRepository: TodoRepository) : UpsertTodoUseCase {        return UpsertTodoUseCase(todoRepository , Dispatchers.IO)    }    @Singleton    @Provides    fun provideDeleteTodoUseCase(todoRepository: TodoRepository) : DeleteTodoUseCase {        return DeleteTodoUseCase(todoRepository , Dispatchers.IO)    }    @Singleton    @Provides    fun provideGetTodoListUseCase(todoRepository: TodoRepository) : GetTodoListUseCase {        return GetTodoListUseCase(todoRepository , Dispatchers.IO)    }}
+package ir.mytodo.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ir.mytodo.domain.repositories.TodoRepository
+import ir.mytodo.domain.usecase.UpsertTodoUseCase
+import ir.mytodo.domain.usecase.DeleteTodoUseCase
+import ir.mytodo.domain.usecase.GetTodoListUseCase
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UserCaseModules {
+
+    @Singleton
+    @Provides
+    fun provideUpsertTodoUseCase(todoRepository: TodoRepository) : UpsertTodoUseCase {
+        return UpsertTodoUseCase(todoRepository , Dispatchers.IO)
+    }
+
+    @Singleton
+    @Provides
+    fun provideDeleteTodoUseCase(todoRepository: TodoRepository) : DeleteTodoUseCase {
+        return DeleteTodoUseCase(todoRepository , Dispatchers.IO)
+    }
+
+    @Singleton
+    @Provides
+    fun provideGetTodoListUseCase(todoRepository: TodoRepository) : GetTodoListUseCase {
+        return GetTodoListUseCase(todoRepository , Dispatchers.IO)
+    }
+
+}
