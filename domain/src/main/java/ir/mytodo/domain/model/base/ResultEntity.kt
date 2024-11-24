660f7846409b8172b326769dc49f39802b9add4e
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:36:24 +0330

    Update ResultEntity.kt

--- domain/src/main/java/ir/mytodo/domain/model/base/ResultEntity.kt
+++ domain/src/main/java/ir/mytodo/domain/model/base/ResultEntity.kt
@@ -1,1 +1,6 @@
-package ir.mytodo.domain.model.basesealed class ResultEntity<out R> {    data class Success<out T>(val data: T) : ResultEntity<T>()    data class Error(var error: String) : ResultEntity<Nothing>()}
+package ir.mytodo.domain.model.base
+
+sealed class ResultEntity<out R> {
+    data class Success<out T>(val data: T) : ResultEntity<T>()
+    data class Error(var error: String) : ResultEntity<Nothing>()
+}
