e83e296105955e254677f01aa9c51b3a2285c20f
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:36:08 +0330

    Update Todo.kt

--- domain/src/main/java/ir/mytodo/domain/model/Todo.kt
+++ domain/src/main/java/ir/mytodo/domain/model/Todo.kt
@@ -1,1 +1,11 @@
-package ir.mytodo.domain.modelimport ir.mytodo.domain.enums.TodoPrioritydata class Todo(    val id : Long ,    val title : String ,    val description : String ,    val iconName : String ,    val priority: TodoPriority = TodoPriority.Medium)
+package ir.mytodo.domain.model
+
+import ir.mytodo.domain.enums.TodoPriority
+
+data class Todo(
+    val id : Long ,
+    val title : String ,
+    val description : String ,
+    val iconName : String ,
+    val priority: TodoPriority = TodoPriority.Medium
+)
