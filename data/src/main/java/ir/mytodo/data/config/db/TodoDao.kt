e412ac00bb5e0f3da79fe7b30dbda6344248fa54
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:34:29 +0330

    Update TodoDao.kt

--- data/src/main/java/ir/mytodo/data/config/db/TodoDao.kt
+++ data/src/main/java/ir/mytodo/data/config/db/TodoDao.kt
@@ -1,1 +1,21 @@
-package ir.mytodo.data.config.dbimport androidx.room.Daoimport androidx.room.Deleteimport androidx.room.Queryimport androidx.room.Updateimport androidx.room.Upsertimport ir.mytodo.data.entities.TodoEntity@Daointerface TodoDao {    @Upsert(TodoEntity::class)    suspend fun upsert(todo: TodoEntity) : Long    @Delete(TodoEntity::class)    suspend fun delete(todo: TodoEntity)    @Query("SELECT * FROM todo ORDER BY id DESC")    suspend fun getList() : List<TodoEntity>}
+package ir.mytodo.data.config.db
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Update
+import androidx.room.Upsert
+import ir.mytodo.data.entities.TodoEntity
+
+@Dao
+interface TodoDao {
+
+    @Upsert(TodoEntity::class)
+    suspend fun upsert(todo: TodoEntity) : Long
+
+    @Delete(TodoEntity::class)
+    suspend fun delete(todo: TodoEntity)
+
+    @Query("SELECT * FROM todo ORDER BY id DESC")
+    suspend fun getList() : List<TodoEntity>
+}
