c882eb241571ff40229dace54bc631a57c3bb695
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:31:51 +0330

    Update CustomComposePreview.kt

--- app/src/main/java/ir/mytodo/presentation/utilities/CustomComposePreview.kt
+++ app/src/main/java/ir/mytodo/presentation/utilities/CustomComposePreview.kt
@@ -1,1 +1,9 @@
-package ir.mytodo.presentation.utilitiesimport android.content.res.Configurationimport androidx.compose.runtime.Composableimport androidx.compose.ui.tooling.preview.Preview@Preview(showBackground = true , uiMode = Configuration.UI_MODE_NIGHT_YES)@Preview(showBackground = true , uiMode = Configuration.UI_MODE_NIGHT_NO)annotation class CustomComposePreview()
+package ir.mytodo.presentation.utilities
+
+import android.content.res.Configuration
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true , uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true , uiMode = Configuration.UI_MODE_NIGHT_NO)
+annotation class CustomComposePreview()
