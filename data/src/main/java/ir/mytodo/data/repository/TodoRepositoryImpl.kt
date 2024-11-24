455162f67394fa0686ec0ed317cc1b37b481af9e
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:33:18 +0330

    Update TodoRepositoryImpl.kt

--- data/src/main/java/ir/mytodo/data/repository/TodoRepositoryImpl.kt
+++ data/src/main/java/ir/mytodo/data/repository/TodoRepositoryImpl.kt
@@ -1,1 +1,25 @@
-package ir.mytodo.data.repositoryimport ir.mytodo.domain.model.Todoimport ir.mytodo.domain.model.base.ResultEntityimport ir.mytodo.domain.repositories.TodoLocalDataSourceimport ir.mytodo.domain.repositories.TodoRepositoryimport kotlinx.coroutines.flow.Flowclass TodoRepositoryImpl(    private val todoDataSource: TodoLocalDataSource) : TodoRepository {    override fun upsert(todo: Todo): Flow<ResultEntity<Long>> {        return todoDataSource.upsert(todo)    }    override fun getList(): Flow<ResultEntity<List<Todo>>> {        return todoDataSource.getList()    }    override fun delete(todo: Todo): Flow<ResultEntity<Unit>> {        return todoDataSource.delete(todo)    }}
+package ir.mytodo.data.repository
+
+import ir.mytodo.domain.model.Todo
+import ir.mytodo.domain.model.base.ResultEntity
+import ir.mytodo.domain.repositories.TodoLocalDataSource
+import ir.mytodo.domain.repositories.TodoRepository
+import kotlinx.coroutines.flow.Flow
+
+class TodoRepositoryImpl(
+    private val todoDataSource: TodoLocalDataSource
+) : TodoRepository {
+
+    override fun upsert(todo: Todo): Flow<ResultEntity<Long>> {
+        return todoDataSource.upsert(todo)
+    }
+
+    override fun getList(): Flow<ResultEntity<List<Todo>>> {
+        return todoDataSource.getList()
+    }
+
+    override fun delete(todo: Todo): Flow<ResultEntity<Unit>> {
+        return todoDataSource.delete(todo)
+    }
+
+}
