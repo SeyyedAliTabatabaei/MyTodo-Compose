57c0ce5cc2cd7d3fbcf8effec9da56301df2e6b5
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:34:16 +0330

    Update BaseDataSource.kt

--- data/src/main/java/ir/mytodo/data/datasource/BaseDataSource.kt
+++ data/src/main/java/ir/mytodo/data/datasource/BaseDataSource.kt
@@ -1,1 +1,19 @@
-package ir.mytodo.data.datasourceimport androidx.core.os.BuildCompatimport ir.mytodo.domain.model.base.ResultEntityabstract class BaseDataSource {    suspend fun <T> safeQueryDb(query : suspend () -> T) : ResultEntity<T> {        return try {            query.invoke().let {                ResultEntity.Success(it)            }        } catch (e : Exception) {            ResultEntity.Error(e.message.toString())        }    }}
+package ir.mytodo.data.datasource
+
+import androidx.core.os.BuildCompat
+import ir.mytodo.domain.model.base.ResultEntity
+
+abstract class BaseDataSource {
+
+    suspend fun <T> safeQueryDb(query : suspend () -> T) : ResultEntity<T> {
+        return try {
+            query.invoke().let {
+                ResultEntity.Success(it)
+            }
+        } catch (e : Exception) {
+            ResultEntity.Error(e.message.toString())
+        }
+    }
+
+
+}
