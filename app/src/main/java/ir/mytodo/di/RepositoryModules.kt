444f045f36d445d30e52685c98e98f8627a75aee
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:29:19 +0330

    Update RepositoryModules.kt

--- app/src/main/java/ir/mytodo/di/RepositoryModules.kt
+++ app/src/main/java/ir/mytodo/di/RepositoryModules.kt
@@ -1,1 +1,22 @@
-package ir.mytodo.diimport dagger.Moduleimport dagger.Providesimport dagger.hilt.InstallInimport dagger.hilt.components.SingletonComponentimport ir.mytodo.data.repository.TodoRepositoryImplimport ir.mytodo.domain.repositories.TodoLocalDataSourceimport ir.mytodo.domain.repositories.TodoRepositoryimport javax.inject.Singleton@Module@InstallIn(SingletonComponent::class)object RepositoryModules {    @Singleton    @Provides    fun provideTodoRepository(todoDataSource: TodoLocalDataSource) : TodoRepository {        return TodoRepositoryImpl(todoDataSource)    }}
+package ir.mytodo.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ir.mytodo.data.repository.TodoRepositoryImpl
+import ir.mytodo.domain.repositories.TodoLocalDataSource
+import ir.mytodo.domain.repositories.TodoRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModules {
+
+    @Singleton
+    @Provides
+    fun provideTodoRepository(todoDataSource: TodoLocalDataSource) : TodoRepository {
+        return TodoRepositoryImpl(todoDataSource)
+    }
+
+}
