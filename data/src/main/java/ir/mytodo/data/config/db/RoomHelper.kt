b04a750271c5a0adb4ac97af12aee49ae8b21142
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:34:42 +0330

    Update RoomHelper.kt

--- data/src/main/java/ir/mytodo/data/config/db/RoomHelper.kt
+++ data/src/main/java/ir/mytodo/data/config/db/RoomHelper.kt
@@ -1,1 +1,16 @@
-package ir.mytodo.data.config.dbimport android.content.Contextimport androidx.room.Roomimport androidx.room.RoomDatabaseimport ir.mytodo.data.Constantsclass RoomHelper {    operator fun invoke(context: Context) : AppDataBase {        return Room            .databaseBuilder(context , AppDataBase::class.java , Constants.DB_NAME)            .build()    }}
+package ir.mytodo.data.config.db
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ir.mytodo.data.Constants
+
+class RoomHelper {
+
+    operator fun invoke(context: Context) : AppDataBase {
+        return Room
+            .databaseBuilder(context , AppDataBase::class.java , Constants.DB_NAME)
+            .build()
+    }
+
+}
