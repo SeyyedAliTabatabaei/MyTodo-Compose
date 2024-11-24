04059b749ccaa42d7694e55b2f4fe8f13e8caf49
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:28:41 +0330

    Update AppModules.kt

--- app/src/main/java/ir/mytodo/di/AppModules.kt
+++ app/src/main/java/ir/mytodo/di/AppModules.kt
@@ -1,1 +1,30 @@
-package ir.mytodo.diimport android.content.Contextimport dagger.Moduleimport dagger.Providesimport dagger.hilt.InstallInimport dagger.hilt.android.qualifiers.ApplicationContextimport dagger.hilt.components.SingletonComponentimport ir.mytodo.data.config.db.AppDataBaseimport ir.mytodo.data.config.db.RoomHelperimport ir.mytodo.data.config.db.TodoDaoimport javax.inject.Singleton@Module@InstallIn(SingletonComponent::class)object AppModules {    @Singleton    @Provides    fun provideAppDataBase(@ApplicationContext context: Context) : AppDataBase {        return RoomHelper().invoke(context)    }    @Singleton    @Provides    fun provideDao(appDataBase: AppDataBase) : TodoDao {        return appDataBase.getDao()    }}
+package ir.mytodo.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import ir.mytodo.data.config.db.AppDataBase
+import ir.mytodo.data.config.db.RoomHelper
+import ir.mytodo.data.config.db.TodoDao
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModules {
+
+    @Singleton
+    @Provides
+    fun provideAppDataBase(@ApplicationContext context: Context) : AppDataBase {
+        return RoomHelper().invoke(context)
+    }
+
+    @Singleton
+    @Provides
+    fun provideDao(appDataBase: AppDataBase) : TodoDao {
+        return appDataBase.getDao()
+    }
+
+}
