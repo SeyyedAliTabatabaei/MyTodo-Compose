39350da5039fa8df5e957b96b507dd5e465169b9
Seyyed Ali Tabatabaei <49515858+SeyyedAliTabatabaei@users.noreply.github.com>
2024-11-24 19:28:52 +0330

    Update DataSourceModules.kt

--- app/src/main/java/ir/mytodo/di/DataSourceModules.kt
+++ app/src/main/java/ir/mytodo/di/DataSourceModules.kt
@@ -1,1 +1,21 @@
-package ir.mytodo.diimport dagger.Moduleimport dagger.Providesimport dagger.hilt.InstallInimport dagger.hilt.components.SingletonComponentimport ir.mytodo.data.config.db.TodoDaoimport ir.mytodo.data.entities.mappers.TodoMappersimport javax.inject.Singleton@Module@InstallIn(SingletonComponent::class)object DataSourceModules {    @Singleton    @Provides    fun provideTodoDataSource(todoDao: TodoDao , todoMappers: TodoMappers) : ir.mytodo.domain.repositories.TodoLocalDataSource {        return ir.mytodo.data.datasource.TodoLocalDataSource(todoDao , todoMappers)    }}
+package ir.mytodo.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ir.mytodo.data.config.db.TodoDao
+import ir.mytodo.data.entities.mappers.TodoMappers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataSourceModules {
+
+    @Singleton
+    @Provides
+    fun provideTodoDataSource(todoDao: TodoDao , todoMappers: TodoMappers) : ir.mytodo.domain.repositories.TodoLocalDataSource {
+        return ir.mytodo.data.datasource.TodoLocalDataSource(todoDao , todoMappers)
+    }
+
+}
